0aaa0e90fae095664de1198e8fd77be15131c5dd
untitled01exe <mfernan4@oswego.edu>
2020-05-06 23:33:07 -0400

    Add files via upload

---
+++ hash/src/com/company/Crypt.java
@@ -0,0 +1,88 @@
+package com.company;
+
+import javax.crypto.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.security.*;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Arrays;
+import java.util.Base64;
+
+public class Crypt {
+    public Cipher c;
+    public KeyPair keyPair;
+
+    public Crypt() throws NoSuchPaddingException, NoSuchAlgorithmException {
+        c = Cipher.getInstance("RSA");
+        KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
+        kpg.initialize(512);
+        keyPair = kpg.genKeyPair();
+    }
+
+    public PublicKey genPublic() throws NoSuchAlgorithmException, InvalidKeySpecException {
+        PublicKey initPublic = this.keyPair.getPublic();
+        KeyFactory k = KeyFactory.getInstance("RSA");
+        byte[] b = initPublic.getEncoded();
+        X509EncodedKeySpec s = new X509EncodedKeySpec(b);
+        PublicKey pk = k.generatePublic(s);
+        return pk;
+    }
+
+    public PrivateKey genSecret() throws NoSuchAlgorithmException, InvalidKeySpecException {
+        PrivateKey initPrivate = this.keyPair.getPrivate();
+        KeyFactory k = KeyFactory.getInstance("RSA");
+        byte[] b = initPrivate.getEncoded();
+        PKCS8EncodedKeySpec s = new PKCS8EncodedKeySpec(b);
+        PrivateKey pk = k.generatePrivate(s);
+        return pk;
+    }
+
+    public byte[] encrypt(PrivateKey key, byte[] data) throws InvalidKeyException, BadPaddingException, IllegalBlockSizeException, IOException {
+        c.init(Cipher.ENCRYPT_MODE, key);
+        byte[] b = c.doFinal(data);
+        //-------------
+        File f = new File("dec.txt");
+        FileOutputStream fo = new FileOutputStream(f);
+        fo.write(b);
+        fo.flush();
+        fo.close();
+        //-------------
+        return b;
+    }
+
+    public byte[] decrypt (PublicKey key, byte[] data) throws InvalidKeyException, BadPaddingException, IllegalBlockSizeException, IOException {
+        c.init(Cipher.DECRYPT_MODE, key);
+        byte[] b = c.doFinal(data);
+        //System.out.println(Arrays.toString(b));
+        //-------------
+        File f = new File("out.txt");
+        FileOutputStream fo = new FileOutputStream(f);
+        fo.write(b);
+        fo.flush();
+        fo.close();
+        //-------------
+        return b;
+    }
+
+    //encryptedString = encryptedString.replaceAll("(?:\\r\\n|\\n\\r|\\n|\\r)", "")
+
+    public static void main(String[] args) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeySpecException, BadPaddingException, InvalidKeyException, IllegalBlockSizeException, IOException {
+        Crypt c = new Crypt();
+        byte[] b;
+
+        PrivateKey sk = c.genSecret();
+        PublicKey pk = c.genPublic();
+        System.out.println();
+        System.out.println(Arrays.toString(sk.getEncoded()));
+        System.out.println(Arrays.toString(pk.getEncoded()));
+
+        b = c.encrypt(c.genSecret(), "WOAH I GOTTA GET IN THE BOX".getBytes());
+        c.decrypt(c.genPublic(), b);
+    }
+}
+
+//https://mkyong.com/java/java-asymmetric-cryptography-example/
---
+++ hash/src/com/company/TransactionBuilder.java
@@ -0,0 +1,145 @@
+package com.company;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class TransactionBuilder {
+
+    String space = "\n\n";
+
+    File f;
+
+    USERS users = new USERS();
+
+    Hash h = new Hash();
+
+    public TransactionBuilder() throws InvalidKeySpecException, NoSuchAlgorithmException {
+        f = new File("chain.txt");
+    }
+
+    public String initBlock() throws NoSuchAlgorithmException {
+        String t1 = "0 S " + users.users[0].username + " 20";
+        String t2 = "0 S " + users.users[1].username + " 20";
+        String t3 = "0 S " + users.users[2].username + " 20";
+        String t4 = "0 S " + users.users[3].username + " 20";
+        //init users
+        for (User user : users.users) {
+            user.addFunds(20);
+            }
+        //add "coin" to each user instance
+        String totals = users.totals();
+        //add the totals line to the block string
+        String block = "BLOCK: 0" + space + t1 + space + t2 + space + t3 + space + t4 + space + "*t5*" + space + totals + space + "old key" + space;
+        System.out.println();
+
+        String ret = h.mineTrialwString(block);
+        //finds an appropriate key to complete the block
+        return ret + space;
+    }
+
+    public String randomBlock() throws FileNotFoundException, NoSuchAlgorithmException {
+        File f = new File("chain.txt");
+        Scanner s = new Scanner(f);
+        s.useDelimiter(Pattern.compile("\nBLOCK: ([0-9])+\n"));
+        int index = -1;
+        while(s.hasNext()){
+            s.next();
+            index++;
+        }
+        //^^ finds current block number
+        int transIndex = index*4+1;
+        //finds current transaction number
+        int lineIndex = index * 19 + 12;
+        //finds the important stuff on the current block
+        s = new Scanner(f);
+
+        for(int i = 0; i < lineIndex; i++){
+            s.nextLine();
+        }
+        //gets to current block
+
+        parseTotalsLine(s.nextLine());
+        //reconfigures coin data for all users using last totals line
+
+        for(int i = 0; i < 3; i++){
+            s.nextLine();
+        }
+
+        String oldHashKey = s.nextLine();
+        //gets the last block's key
+
+        String t1 = buildTransactionLine(transIndex);
+        transIndex++;
+        String t2 = buildTransactionLine(transIndex);
+        transIndex++;
+        String t3 = buildTransactionLine(transIndex);
+        transIndex++;
+        String t4 = buildTransactionLine(transIndex);
+        transIndex++;
+        //creates random transactions
+        Random r = new Random();
+        String t5 = buildRewardLine(users.users[r.nextInt(users.users.length)], transIndex);
+        //reward coin transaction
+
+        String totals = users.totals();
+
+        String block = "BLOCK: " + (index+1) + space + t1 + space + t2 + space + t3 + space + t4 + space + t5 + space + totals + space + oldHashKey + space;
+        //constructs block string
+
+        String ret = h.mineTrialwString(block);
+        //System.out.println(index);
+        return ret + space;
+    }
+
+    public void parseTotalsLine(String line){
+        Scanner s = new Scanner(line);
+        s.useDelimiter(" ");
+        //uses space delimiter to get each user/coin pair
+        while(s.hasNext()) {
+            User u = users.getUser(s.next());
+            int coin = Integer.parseInt(s.next());
+            u.coin = coin;
+        }
+    }
+
+    public String buildTransactionLine(int trans) {
+        User u1 = null;
+        User u2 = null;
+        Random r = new Random();
+        boolean flag = true;
+        //finds 2 users to send/receive coins
+        while (flag){
+             u1 = users.users[r.nextInt(users.users.length)];
+             u2 = users.users[r.nextInt(users.users.length)];  //<<A bug
+            if(u1.coin > 0){
+                flag = false;
+            }
+        }
+
+        int transferCoin = r.nextInt(r.nextInt(u1.coin)); //<< Bug??
+        u1.removeFunds(transferCoin);
+        u2.addFunds(transferCoin);
+        //replaces the necessary funds for each user
+
+        String ret = trans + " " + u1.username + " " + transferCoin + " " + u2.username;
+        return ret;
+    }
+
+    public String buildRewardLine(User u, int tranNum){
+        u.addFunds(20);
+        return tranNum + " S 20 " + u.username;
+        //another call to increase funds for init block
+    }
+}
